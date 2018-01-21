294bc828207f40feb9058f05a689f8e38c3578a0
BarneyRM <35643911+BarneyRM@users.noreply.github.com>
2018-01-21 00:20:56 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./miner --server  us.hushmine.pro --user t1VoR8JXjCPY2iSwFYdA8zVrrgKEvxeJEXH.createthis/YOUR_EMAIL --pec --fee 0 --pass z --port 9009
+./miner --server  eth-eu1.nanopool.org:9999 --user 0x87894D5793E663Ef6d586cCB51bbe2E4f92DfE05.BARNEYLINUX/barney.ellam@live.com --pec --fee 0 --pass z --port 9999
